d9a78734b81ef79cff16de6c2c80557527bad448
Clarence Adrias <clarence.adrias@gmail.com>
2025-11-26 19:21:53 +0800

    update python path for local

--- FaceShapeAI/config.php
+++ FaceShapeAI/config.php
@@ -5,9 +5,9 @@
 // -------------------------------------------
 
 // OPTION 1: Use virtual environment Python (recommended)
-//$pythonPath = __DIR__ . '/.venv/Scripts/python.exe';
+$pythonPath = __DIR__ . '/.venv/Scripts/python.exe';
 
-$pythonPath = '/var/www/BeautyGo/FaceShapeAI/venv311/bin/python';
+// $pythonPath = '/var/www/BeautyGo/FaceShapeAI/venv311/bin/python';
 
 
 // OPTION 2: Use sysstem Python instead (only if venv is not used)
